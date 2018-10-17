2a1bf688272507a348ef6bc394933f24365083dc
William Bellavance <flintman@flintmancomputers.com>
2018-10-17 10:36:21 -0400

    fix up ion_handle_get_by_id as it was removed
    from kernel.org updates
    Below is the commit that required the this change
    commit 3e53a28cf93779cbca8a008773b09e88e8b4b453
    
    Change-Id: Ida279719a9eaa5fea9042f70127031f83f6c91a4

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -499,7 +499,7 @@ static struct ion_handle *ion_handle_lookup(struct ion_client *client,
 	return ERR_PTR(-EINVAL);
 }
 
-static struct ion_handle *ion_handle_get_by_id_nolock(struct ion_client *client,
+struct ion_handle *ion_handle_get_by_id_nolock(struct ion_client *client,
 						int id)
 {
 	struct ion_handle *handle;
